504fd5bf3a40648682e9398637b2625ca61ebdee
manoruiz <manoruizfoto@gmail.com>
2022-06-15 13:04:52 +0200

    qualiaimages

--- script.js
+++
@@ -1,8 +0,0 @@
-function darkmode() {
-document.querySelector("body").style.backgroundColor = "black";
-document.querySelector("body").style.color = "white";
-}
-function lightmode() {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-}

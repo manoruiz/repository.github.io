d2660fe328350e9480692cf51ccfe664bf64a49c
manoruiz <manoruizfoto@gmail.com>
2022-06-15 12:22:40 +0200

    commit en vsc

---
+++ script.js
@@ -0,0 +1,8 @@
+function darkmode() {
+document.querySelector("body").style.backgroundColor = "black";
+document.querySelector("body").style.color = "white";
+}
+function lightmode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
